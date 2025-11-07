0bbb79591de521aba320eb893d5f59d0a77e2b03
sadhu1437 <146161827+sadhu1437@users.noreply.github.com>
2025-11-07 10:05:31 +0530

    Update Driver.java

--- src/main/java/com/cognizant/makemytrip1/base/Driver.java
+++ src/main/java/com/cognizant/makemytrip1/base/Driver.java
@@ -1,11 +1,12 @@
 package com.cognizant.makemytrip1.base;
 
-import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
-import org.openqa.selenium.io.FileHandler;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
@@ -14,23 +15,28 @@ public class Driver {
     public static WebDriver driver;
     public static WebDriverWait wait;
     public static final String url = "https://www.makemytrip.com/";
+    public static final String hubURL = "http://10.194.187.144:4444/wd/hub"; // Hub endpoint
 
     @BeforeSuite
     public void setupDriver() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(url);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@data-cy='closeModal']"))).click();
-        System.out.println("Browser launched and MakeMyTrip homepage opened.");
-    }
-    
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("chrome"); // or "firefox" if your node supports it
 
-    @AfterSuite
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-            System.out.println("Browser closed after suite execution.");
+            driver = new RemoteWebDriver(new URL(hubURL), capabilities);
+            driver.manage().window().maximize();
+            driver.get(url);
+
+            wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@data-cy='closeModal']"))).click();
+
+            System.out.println("Browser launched via Selenium Grid and MakeMyTrip homepage opened.");
+        } catch (MalformedURLException e) {
+            System.err.println("Invalid Hub URL: " + hubURL);
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Error during driver setup: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
